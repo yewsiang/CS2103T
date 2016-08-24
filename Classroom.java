28607063ef04ac946492fd937fc42a0aa4d2aa94
yewsiang <tangys108@gmail.com>
2016-08-24 10:44:48 +0800

    Asks for number for students

--- Classroom.java
+++ Classroom.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public Classroom {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Good morning students!");
+
+        System.out.println("How many students are there?");
+        int numStudents = sc.nextInt();
+        System.out.println("Number of students: " + numStudents);
     }
 }
